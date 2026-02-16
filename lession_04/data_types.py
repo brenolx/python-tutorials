72493f76e07c062130cda9c2897ca3561ce70bd0
brenolx <brenosam22@gmail.com>
2026-02-15 21:44:20 -0300

    adicionando arquivo lession_04

---
+++ lession_04/data_types.py
@@ -0,0 +1,48 @@
+# String data type
+
+# literal assignment
+nome = "Breno"
+sobrenome = "Cavalcante"
+
+# Tipo String
+# print(type(nome))
+# print(type(nome) == str)  # True
+# print(isinstance(nome, str))  # True, é uma instancia da classe string
+
+# Construcor function str()
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# Concatenando strings
+# fullname = nome + " " + sobrenome
+# print(fullname)
+
+# fullname += "!"
+# print(fullname)
+
+# Casting de um numero para string
+# decade = str(1980)
+
+# print(type(decade))
+# print(decade)
+
+# statement = "I like rock music from the " + decade + "s."
+# print(statement)
+
+# Multipas Linhas
+# multiline = '''
+# Hey, how are you?
+
+# I was just checking in..
+#                             All good?
+
+# '''
+# print(multiline)
+
+# Caractere especial de escape
+frase = 'I\'m back at work! \t Hey!\n\nWhere\'s this at\\located?'
+print(frase)
+
+# String methods
