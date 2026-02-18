4a442507553839863da2d3de21f3c67ad5abe977
brenolx <brenosam22@gmail.com>
2026-02-18 16:37:01 -0300

    alterando lession_04

--- lession_04/data_types.py
+++ lession_04/data_types.py
@@ -1,28 +1,32 @@
 # String data type
 
 # literal assignment
-nome = "Breno"
-sobrenome = "Cavalcante"
+first = "Breno"
+last = "Cavalcante"
 
-# Tipo String
-# print(type(nome))
-# print(type(nome) == str)  # True
-# print(isinstance(nome, str))  # True, é uma instancia da classe string
+# Tipo String ---------------------------------------------------------------------
 
-# Construcor function str()
+# print(type(first))
+# print(type(first) == str)  # True
+# print(isinstance(first, str))  # True, é uma instancia da classe string
+
+# Construcor function str() -------------------------------------------------------
+
 # pizza = str("Pepperoni")
 # print(type(pizza))
 # print(type(pizza) == str)
 # print(isinstance(pizza, str))
 
-# Concatenando strings
-# fullname = nome + " " + sobrenome
+# Concatenando strings -------------------------------------------------------------
+
+# fullname = first + " " + last
 # print(fullname)
 
 # fullname += "!"
 # print(fullname)
 
-# Casting de um numero para string
+# Casting de um numero para string -------------------------------------------------
+
 # decade = str(1980)
 
 # print(type(decade))
@@ -31,8 +35,8 @@ sobrenome = "Cavalcante"
 # statement = "I like rock music from the " + decade + "s."
 # print(statement)
 
-# Multipas Linhas
-# multiline = '''
+# Multipas Linhas -----------------------------------------------------------
+multiline = '''
 # Hey, how are you?
 
 # I was just checking in..
@@ -42,7 +46,33 @@ sobrenome = "Cavalcante"
 # print(multiline)
 
 # Caractere especial de escape
-frase = 'I\'m back at work! \t Hey!\n\nWhere\'s this at\\located?'
-print(frase)
+#frase = 'I\'m back at work!\t Hey!\n\nWhere\'s this at\\located?'
+#print(frase)
 
-# String methods
+# String methods ---------------------------------------------------------
+
+# print(first)
+# print(first.lower())
+# print(first.upper())
+# print(first)
+
+#print(multiline.title()) # deixa a primeira letra da palavara Maiúscula
+#print(multiline.replace("good", "ok")) # substitui a palavra fornecida
+#print(multiline)
+
+#texto_com_espaco = "     hello world!     "
+
+#print(len(texto_com_espaco))
+#print(len(texto_com_espaco.strip())) # remove os espaços em branco
+#print(len(texto_com_espaco.lstrip())) # remove os espaços do lado esquerdo
+#print(len(texto_com_espaco.rstrip())) # remove os espaços do lado direito
+
+# Construindo um menu ------------------------------------------------------------
+
+titulo = "menu".upper() # deixa todos caracteres maiusculos
+print(titulo.center(20, "=")) # centralisa a variavel e preenche com simbolo
+print("Café".ljust(16, ".") + "1$".rjust(4))
+print("Pão de Queijo".ljust(16, ".") + "1.2$".rjust(4)) # justifica esquerda ljust(), justifica direita rjust()
+print("Misto Quente".ljust(16, ".") + "2.5$".rjust(4))  # justifica esquerda ljust(), justifica direita rjust()
+
+# Índice de String 
