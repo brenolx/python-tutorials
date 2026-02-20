c4060c6135da24fe587ac892e3fe36f1d8b72476
brenolx <brenosam22@gmail.com>
2026-02-20 18:31:22 -0300

    concluindo arquivo lession_04

--- lession_04/data_types.py
+++ lession_04/data_types.py
@@ -1,6 +1,7 @@
 # String data type
 
 # literal assignment
+import math
 first = "Breno"
 last = "Cavalcante"
 
@@ -36,18 +37,18 @@ last = "Cavalcante"
 # print(statement)
 
 # Multipas Linhas -----------------------------------------------------------
-multiline = '''
-# Hey, how are you?
+# multiline = '''
+# # Hey, how are you?
 
-# I was just checking in..
-#                             All good?
+# # I was just checking in..
+# #                             All good?
 
-# '''
+# # '''
 # print(multiline)
 
 # Caractere especial de escape
-#frase = 'I\'m back at work!\t Hey!\n\nWhere\'s this at\\located?'
-#print(frase)
+# frase = 'I\'m back at work!\t Hey!\n\nWhere\'s this at\\located?'
+# print(frase)
 
 # String methods ---------------------------------------------------------
 
@@ -56,23 +57,80 @@ multiline = '''
 # print(first.upper())
 # print(first)
 
-#print(multiline.title()) # deixa a primeira letra da palavara Maiúscula
-#print(multiline.replace("good", "ok")) # substitui a palavra fornecida
-#print(multiline)
+# print(multiline.title()) # deixa a primeira letra da palavara Maiúscula
+# print(multiline.replace("good", "ok")) # substitui a palavra fornecida
+# print(multiline)
 
-#texto_com_espaco = "     hello world!     "
+# texto_com_espaco = "     hello world!     "
 
-#print(len(texto_com_espaco))
-#print(len(texto_com_espaco.strip())) # remove os espaços em branco
-#print(len(texto_com_espaco.lstrip())) # remove os espaços do lado esquerdo
-#print(len(texto_com_espaco.rstrip())) # remove os espaços do lado direito
+# print(len(texto_com_espaco)) # exibe o tamanho total da string incluindo os espaços
+# print(len(texto_com_espaco.strip())) # remove os espaços em branco
+# print(len(texto_com_espaco.lstrip())) # remove os espaços do lado esquerdo
+# print(len(texto_com_espaco.rstrip())) # remove os espaços do lado direito
 
 # Construindo um menu ------------------------------------------------------------
 
-titulo = "menu".upper() # deixa todos caracteres maiusculos
-print(titulo.center(20, "=")) # centralisa a variavel e preenche com simbolo
-print("Café".ljust(16, ".") + "1$".rjust(4))
-print("Pão de Queijo".ljust(16, ".") + "1.2$".rjust(4)) # justifica esquerda ljust(), justifica direita rjust()
-print("Misto Quente".ljust(16, ".") + "2.5$".rjust(4))  # justifica esquerda ljust(), justifica direita rjust()
+# titulo = "menu".upper()  # deixa todos caracteres maiusculos
+# print(titulo.center(20, "="))  # centralisa a variavel e preenche com simbolo
+# print("Café".ljust(16, ".") + "1$".rjust(4))
 
-# Índice de String 
+# justifica esquerda ljust(), justifica direita rjust()
+# print("Pão de Queijo".ljust(16, ".") + "1.2$".rjust(4))
+
+# justifica esquerda ljust(), justifica direita rjust()
+# print("Misto Quente".ljust(16, ".") + "2.5$".rjust(4))
+
+# Índice de String --------------------------------------------------------------
+
+# print(first[1:-1])  # exibe o segundo e o antepenúltimo
+# print(first[0:])  # percorre e exibe do inicio até o fim da string
+
+# Métodos que retornam boleano
+# a string começa com a letra "B" (sensitive case)
+# print(first.startswith("B"))
+# a string termina com a letra "a" (sensitive case)
+# print(first.endswith("a"))
+
+# Tipos de dados boleano -------------------------------------------------------
+valor = True
+x = bool(False)  # função construtora
+
+print("É uma instância: ", isinstance(valor, bool))
+print(type(valor), type(x))
+print(valor, x)
+
+# tipo int ---------------------------------------------------------------------
+preco = 100
+melhor_preco = int(80)
+print(type(preco))
+print(isinstance(melhor_preco, int))
+
+# tipo float ---------------------------------------------------------------------
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+print(isinstance(y, float))
+
+# tipo complex -------------------------------------------------------------------
+comp_value = 5+3j
+print(type(comp_value))
+print(comp_value.real)  # valor real
+print(comp_value.imag)  # valor imaginario
+
+# Built-in functions for numbers --------------------------------------------------
+print("\n" + 100 * "=")
+print(abs(gpa))  # função para obter um valor absoluto de um numero
+print(round(gpa))  # função que arredonda o valor para o inteiro mais proximo
+print(round(gpa, 1))  # podemos passar o numero de casas por parâmetro
+
+# Modulo math ---------------------------------------------------------------------
+print("\n" + 100 * "=")
+print(math.pi)
+print(math.sqrt(64))
+print(math.pow(3, 3))
+
+# Casting de string para inteiro ---------------------------------------------------
+print("\n" + 100 * "=")
+CEP_STRING = "10001"
+CEP_INT = int(CEP_STRING)
+print(CEP_INT, type(CEP_INT))
